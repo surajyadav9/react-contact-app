de3cab943346687b03ac1897fc7fe4be5900aa00
Suraj Yadav <surajyadav90388@gmail.com>
2022-01-30 21:30:12 +0530

    CRUD

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import './App.css'
 import Header from './Header'
 import AddContact from './AddContact'
-import ContactCard from './ContactCard'
 import ContactList from './ContactList'
-import './App.css'
+
 
 function App() {
+
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([])
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, { id: uuidv4(), ...contact }])
+  }
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className='ui container'>
       <Header/>
-      <AddContact/>
-      <ContactList/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} removeContactId={removeContactHandler} />
       {/* <ContactCard/> */}
     </div>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React from "react"
+import ContactCard from "./ContactCard"
 
-const ContactList = () => {
+
+const ContactList = (props) => {
+
+    const deleteConactHandler = (id) => {
+        props.removeContactId(id);
+    };
+
+    const renderContactList = props.contacts.map((contact)=>{
+        return (
+            <ContactCard contact={contact}
+            clickHander={deleteConactHandler}
+            key={contact.id}
+            />
+        )
+    })
+
     return (
-        <div className="ui celled list">Contact List</div>
+        <div className="ui celled list">{renderContactList}</div>
     )
 }
 
